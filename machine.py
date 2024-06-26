0c61fa8a3959cc8c6e2663e5f3971e62404df1ce
viggope <59804534+viggope@users.noreply.github.com>
2024-06-26 20:14:40 +0200

    Updated machine.py
    Added ifs that can only execute one line of code when statement is True.

--- machine.py
+++ machine.py
@@ -1,9 +1,16 @@
 # from sourceCode import *
+import commands
 from commands import *
+
+
+currentLine_ = commands.currentLine
+using = {"2D": False, "scanner": False, "input": False}
 
 
 def runLine(line: int):
     global preStored
+    if preStored["debug"]["value"]:
+        print(storage, "STORAGE")
     lineCont = lines[line]
     lineStrings = lineCont.split("\"")
     lineStrings2 = []
@@ -22,32 +29,50 @@ def runLine(line: int):
     else:
         lineParts = lineStrings2
     if preStored["debug"]["value"]:
-        print("currentLine:", currentLine, "...")
+        print("\n\ncurrentLine:", currentLine_, "...\n")
         print("lineParts: ", lineParts, "#")
 
     try:
+        if lineParts[0] == "if":
+            if ":" in str(lineParts):
+                index = 0
+                while ":" not in lineParts[index]:
+                    index += 1
+                lineParts = If(lineParts[1:index+1], lineParts[index+1:])
+            else:
+                giveError("syntaxError", "missing :")
+
+
+
         if lineParts[0] == "set":
-            Set(lineParts)
+            Set(lineParts, currentLine_)
 
         elif lineParts[0] == "say":
             Say(lineParts[1])
 
         elif lineParts[0] == "refresh":
             try:
-                exec(Refresh(lineParts))
-            except RecursionError:
-                giveError("depthError")
+                var = storage[lineParts[1]]
+                assignVar(lineParts[1], getVars(var["value"]), var["type"], var["line"])
+            except KeyError:
+                giveError("varNotFound", lineParts[1])
+
+        elif lineParts[0] == "get":
+            try:
+                print(eval(Get(lineParts)))
             except KeyError:
                 giveError("varNotFound", lineParts[1])
 
         elif lineParts[0] == "flip":
             Flip(lineParts[1])
 
-        elif lineParts[0] == "if":
-            pass
-
         elif lineParts[0] == "debug":
-            preStored = Debug(lineParts[1])
+            if lineParts[1] == "flip":
+                preStored = Debug(not preStored["debug"]["value"])
+            elif len(lineParts) == 1:
+                preStored = Debug(True)
+            else:
+                preStored = Debug(lineParts[1])
 
     except IndexError:
         giveError("missingArgument", lineParts[0])
@@ -55,11 +80,11 @@ def runLine(line: int):
 
 
 def runProgram():
-    global debug, currentLine
+    global debug, currentLine_
     for i in range(len(lines)):
         debug = preStored["debug"]["value"]
         runLine(i)
-        currentLine = newLine()
+        currentLine_ = newLine()
 
 
 runProgram()
